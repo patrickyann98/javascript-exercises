8a13ab1ad0ecf8509a746a6aa17bf85dbd4f57ab
patrickyann98 <220473549+patrickyann98@users.noreply.github.com>
2026-02-22 16:19:40 +0000

    Datatypes and String excercises have been done

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -1,11 +1,21 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
+const birthYear = 1947;
+const thisYear = 1965;
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const age = (thisYear - birthYear);
+const fullName = firstName + " " + lastName; 
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
+ const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + age  + " years old.";
+
+console.log(greeting);
+
+module.exports = {
+  greeting,
+  birthYear,
+  thisYear,
+  firstName,
+  lastName
+}
 
 // Add your code right below, good luck!
 
