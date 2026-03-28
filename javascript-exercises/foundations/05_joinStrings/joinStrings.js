7e21122544a04407091cac87400258f90a1d9274
patrickyann98 <220473549+patrickyann98@users.noreply.github.com>
2026-03-28 11:49:56 +0000

    the hole game has been implemented, HTML & CSS are missing.

--- javascript-exercises/foundations/05_joinStrings/joinStrings.js
+++ javascript-exercises/foundations/05_joinStrings/joinStrings.js
@@ -4,7 +4,7 @@ const firstName = "Carlos";
 const lastName = "Stevenson";
 const age = (thisYear - birthYear);
 const fullName = firstName + " " + lastName; 
-
+cd 
  const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + age  + " years old.";
 
 console.log(greeting);
